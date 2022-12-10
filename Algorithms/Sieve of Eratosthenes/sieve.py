3a0cceeaa3b648a009f859862a7971d3ec8b2664
Orfy <aortega0703@gmail.com>
2022-12-10 13:59:50 -0500

    Small optimization to basic Sieve of Eratosthenes

--- Algorithms/Sieve of Eratosthenes/sieve.py
+++ Algorithms/Sieve of Eratosthenes/sieve.py
@@ -1,10 +1,12 @@
 # Set bits are composite while unset bits are prime
 def sieve(N):
     prime = 3 #0b11
-    p = 2
+    # Check divisibility by 2 separately
+    p = 3
     while p**2 <= N:
         if prime[p]:
-            for kp in range(2*p, N+1, p):
+            # Skips all even numbers
+            for kp in range(3*p, N+1, 2*p):
                 # Setting bit kp
                 prime |= 1 << kp
         p += 2
