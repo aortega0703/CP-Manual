276ad4bede60202a927f0871adfcb380e9e79108
Andrés Felipe Ortega Montoya <aortega0703@gmail.com>
2023-02-12 21:15:56 -0500

    Added Binary search leftmost and rightmost

--- code/algorithms/arrays/binary_search.py
+++ code/algorithms/arrays/binary_search.py
@@ -1,12 +1,21 @@
-def bs(q, L):
+def bs_leftmost(q, L):
     l = 0
     r = len(L) - 1
-    while l <= r:
+    while l < r:
         m = (l + r) // 2
-        if L[m] == q:
-            return m
-        elif L[m] < q:
+        if L[m] < q:
             l = m + 1
         else:
-            r = m - 1
-    return -1
+            r = m
+    return l
+
+def bs_rightmost(q, L):
+    l = 0
+    r = len(L) - 1
+    while l < r:
+        m = (l + r) // 2
+        if L[m] > q:
+            r = m
+        else:
+            l = m + 1
+    return r - 1
